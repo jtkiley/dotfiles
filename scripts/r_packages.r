89f5eedd5e6d69e65f5da68d4f0c3ccfb5241cc0
Jason T. Kiley <jkiley@icloud.com>
2015-11-20 14:17:49 -0600

    Added `quantmod` in R.

--- scripts/r_packages.r
+++ scripts/r_packages.r
@@ -15,27 +15,34 @@ options(repos='https://rweb.crmda.ku.edu/cran/')
 #                      DATA HANDLING
 ###########################################################
 
-install.packages("data.table")
-install.packages("doBy")
-install.packages("dplyr")
-install.packages("plyr")
+install.packages('data.table')
+install.packages('doBy')
+install.packages('dplyr')
+install.packages('plyr')
 
-install.packages("sas7bdat")
+install.packages('sas7bdat')
+
+
+###########################################################
+#                    DATA COLLECTION
+###########################################################
+
+install.packages('quantmod')
 
 
 ###########################################################
 #                        GRAPHICS
 ###########################################################
 
-install.packages("ggplot2")
+install.packages('ggplot2')
 
 
 ###########################################################
 #                         MODELS
 ###########################################################
 
-install.packages("lmtest")
-install.packages("pglm")
-install.packages("plm")
-install.packages("sandwich")
-install.packages("Zelig")
+install.packages('lmtest')
+install.packages('pglm')
+install.packages('plm')
+install.packages('sandwich')
+install.packages('Zelig')
